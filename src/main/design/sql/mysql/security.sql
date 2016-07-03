4512ad580da74e677828c1de53303f7b296f43c8
845642761@qq.com <845642761@qq.com>
2016-07-03 16:04:22 +0800

    sql增加注释

--- src/main/design/sql/mysql/security.sql
+++ src/main/design/sql/mysql/security.sql
@@ -3,9 +3,9 @@ USE cms;
 -- 创建用户-角色对应表
 DROP TABLE IF EXISTS `userRole`;
 CREATE TABLE `userRole`(
-	strId							VARCHAR(32)		NOT NULL,											-- 主键Id
-	strLoginId						VARCHAR(50)		NOT NULL,											-- 用户id
-	strRoleId						VARCHAR(32)		NOT NULL,											-- 角色Id
+	strId							VARCHAR(32)		NOT NULL			COMMENT "主键Id",	
+	strLoginId						VARCHAR(50)		NOT NULL			COMMENT "用户id",	
+	strRoleId						VARCHAR(32)		NOT NULL			COMMENT "角色Id",	
 	PRIMARY KEY(strId),
 	UNIQUE(strId)
 )
@@ -15,10 +15,10 @@ DEFAULT CHARACTER SET=utf8;
 -- 创建角色表
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`(
-	strId							VARCHAR(32)		NOT NULL,											-- 主键Id
-	strName							VARCHAR(32)		NOT NULL,											-- 角色名称
-	strDescription					VARCHAR(100)	NOT NULL,											-- 角色描述
-	nState							TINYINT			NOT NULL DEFAULT 0,									-- 状态:0-正常,-1-禁用
+	strId							VARCHAR(32)		NOT NULL			COMMENT "主键Id",	
+	strName							VARCHAR(32)		NOT NULL			COMMENT "角色名称",
+	strDescription					VARCHAR(100)	NOT NULL			COMMENT "角色描述",
+	nState							TINYINT			NOT NULL DEFAULT 0	COMMENT "状态:0-正常,-1-禁用",
 	PRIMARY KEY(strId),
 	UNIQUE(strId)
 )
@@ -28,14 +28,14 @@ DEFAULT CHARACTER SET=utf8;
 -- 创建权限表
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission`(
-	strId							VARCHAR(32)		NOT NULL,											-- 主键Id
-	strPid							VARCHAR(32)		NOT NULL,											-- 父级id
-	strName							VARCHAR(32)		NOT NULL,											-- 权限名称
-	strPermission					VARCHAR(100),														-- 权限标识
-	strDescription					VARCHAR(200),														-- 权限描述
-	nType							TINYINT			NOT NULL DEFAULT 0,									-- 类型:0-菜单,1-按钮
-	nState							TINYINT			NOT NULL DEFAULT 0,									-- 状态:0-正常,-1-禁用
-	dtCreateTime					DATETIME		NOT NULL,											-- 创建时间:yyyy-mm-dd HH:mm:ss
+	strId							VARCHAR(32)		NOT NULL			COMMENT "主键Id",	
+	strPid							VARCHAR(32)		NOT NULL			COMMENT "父级id",	
+	strName							VARCHAR(32)		NOT NULL			COMMENT "权限名称",
+	strPermission					VARCHAR(100)						COMMENT "权限标识",
+	strDescription					VARCHAR(200)						COMMENT "权限描述",
+	nType							TINYINT			NOT NULL DEFAULT 0	COMMENT "类型:0-菜单,1-按钮",
+	nState							TINYINT			NOT NULL DEFAULT 0	COMMENT "状态:0-正常,-1-禁用",
+	dtCreateTime					DATETIME		NOT NULL			COMMENT "创建时间:yyyy-mm-dd HH:mm:ss",
 	PRIMARY KEY(strId),
 	UNIQUE(strId)
 )
@@ -45,9 +45,9 @@ DEFAULT CHARACTER SET=utf8;
 -- 创建角色-权限对应表
 DROP TABLE IF EXISTS `rolePermission`;
 CREATE TABLE `rolePermission`(
-	strId							VARCHAR(32)		NOT NULL,											-- 主键Id
-	strRoleId						VARCHAR(32)		NOT NULL,											-- 角色Id
-	strPermissionId					VARCHAR(32)		NOT NULL,											-- 权限id
+	strId							VARCHAR(32)		NOT NULL			COMMENT "主键Id",	
+	strRoleId						VARCHAR(32)		NOT NULL			COMMENT "角色Id",	
+	strPermissionId					VARCHAR(32)		NOT NULL			COMMENT "权限id",	
 	PRIMARY KEY(strId),
 	UNIQUE(strId)
 )
--- src/main/design/sql/mysql/systemUser.sql
+++ src/main/design/sql/mysql/systemUser.sql
@@ -3,22 +3,23 @@ USE cms;
 -- 创建用户表
 DROP TABLE IF EXISTS `systemUser`;
 CREATE TABLE `systemUser`(
-	strUserId						VARCHAR(50)		NOT NULL,											-- 登录id（字母小写）
-	strPassword						VARCHAR(32)		NOT NULL,											-- 密码
-	strName							VARCHAR(30)		NOT NULL,											-- 姓名
-	nSex							TINYINT			NOT NULL DEFAULT 0,									-- 性别, 0-未填写,1-男,2-女
-	strMobile						VARCHAR(20),														-- 手机号
-	strPhone						VARCHAR(20),														-- 电话
-	strEmail						VARCHAR(60),														-- 邮箱
-	strQQ							VARCHAR(20),														-- QQ
-	strWeChar						VARCHAR(20),														-- 微信
-	dtBirthday						DATETIME,															-- 出生日期:yyyy-mm-dd HH:mm:ss
-	strAddress						VARCHAR(100),														-- 地址
-	strHeadURL						VARCHAR(300),														-- 头像
-	nState							TINYINT			NOT NULL DEFAULT 0,									-- 状态,0-待审核,1-正常,-1-禁用
-	strDeptId						VARCHAR(32)		NOT NULL,											-- 所属部门ID
-	dtCreateTime					DATETIME		NOT NULL,											-- 注册时间:yyyy-mm-dd HH:mm:ss
-	dtUpdateTime					DATETIME		NOT NULL,											-- 修改时间:yyyy-mm-dd HH:mm:ss
+	strUserId						VARCHAR(32)		NOT NULL				COMMENT "id",
+	strLoginName					VARCHAR(50)		NOT NULL				COMMENT "登录账号",
+	strPassword						VARCHAR(32)		NOT NULL				COMMENT "密码",
+	strName							VARCHAR(30)		NOT NULL				COMMENT "姓名",
+	nSex							TINYINT			NOT NULL DEFAULT 0		COMMENT "性别, 0-未填写,1-男,2-女",
+	strMobile						VARCHAR(20)								COMMENT "手机号",
+	strPhone						VARCHAR(20)								COMMENT "电话",
+	strEmail						VARCHAR(60)								COMMENT "邮箱",
+	strQQ							VARCHAR(20)								COMMENT "QQ",
+	strWeChar						VARCHAR(20)								COMMENT "微信",
+	dtBirthday						DATETIME								COMMENT "出生日期:yyyy-mm-dd HH:mm:ss",
+	strAddress						VARCHAR(100)							COMMENT "地址",
+	strHeadURL						VARCHAR(300)							COMMENT "头像",
+	nState							TINYINT			NOT NULL DEFAULT 0		COMMENT "状态,0-待审核,1-正常,-1-禁用",
+	strDeptId						VARCHAR(32)		NOT NULL				COMMENT "所属部门ID",
+	dtCreateTime					DATETIME		NOT NULL				COMMENT "注册时间:yyyy-mm-dd HH:mm:ss",
+	dtUpdateTime					DATETIME		NOT NULL				COMMENT "修改时间:yyyy-mm-dd HH:mm:ss",
 	PRIMARY KEY(strUserId),
 	UNIQUE(strUserId)
 )
