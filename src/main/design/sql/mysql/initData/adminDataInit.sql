3ede41b1c86fe680fee3589cb1e845037a65b429
845642761@qq.com <845642761@qq.com>
2016-07-03 16:12:58 +0800

    初始化admin

--- src/main/design/sql/mysql/initData/adminDataInit.sql
+++ src/main/design/sql/mysql/initData/adminDataInit.sql
@@ -8,25 +8,25 @@ VALUES ('001', '0', '系统管理', NULL, NULL, '0', '0', now()),
 
 -- 初始化角色数据
 INSERT INTO `role` (`strId`, `strName`, `strDescription`, `nState`) 
-VALUES ('001', 'admin', '管理员', '0');
+VALUES ('1', 'admin', '管理员', '0');
 
 -- 初始化角色权限数据
 INSERT INTO `rolepermission` (`strId`, `strRoleId`, `strPermissionId`) 
-VALUES ('001', '001', '001'),
-VALUES ('001', '001', '001-001'),
-VALUES ('001', '001', '001-002'),
+VALUES ('1', '1', '001'),
+VALUES ('2', '1', '001-001'),
+VALUES ('3', '1', '001-002'),
 
 -- 初始化部门数据
 INSERT INTO `department` (`strId`,`strPid`,`strName`,`strDescription`,`nState`,`dtCreateTime`,`nChild`,`strLevel`)
 VALUES('1' ,'0','组织机构' ,'组织机构' ,'0',now() ,'0' ,'001');
 
 -- 初始化用户数据
-INSERT INTO `systemuser` (`strUserId`, `strPassword`, `strName`, `nSex`, `strMobile`, `strPhone`, `strEmail`, `strQQ`, `strWeChar`, `dtBirthday`, `strAddress`, `strHeadURL`, `nState`, `strDeptId`, `dtCreateTime`, `dtUpdateTime`) 
-VALUES ('1', '00000000', 'admin', '0', NULL , NULL, NULL , NULL, NULL , now() , NULL , NULL, '1', '1', now() , now());
+INSERT INTO `systemuser` (`strUserId`,`strLoginName`, `strPassword`, `strName`, `nSex`, `strMobile`, `strPhone`, `strEmail`, `strQQ`, `strWeChar`, `dtBirthday`, `strAddress`, `strHeadURL`, `nState`, `strDeptId`, `dtCreateTime`, `dtUpdateTime`) 
+VALUES ('1', 'admin', '00000000', 'admin', '0', NULL , NULL, NULL , NULL, NULL , now() , NULL , NULL, '1', '1', now() , now());
 
 -- 初始化用户角色数据
 INSERT INTO `userrole` (`strId`, `strLoginId`, `strRoleId`) 
-VALUES ('001', '1', '001');
+VALUES ('1', '1', '1');
 
 
 
