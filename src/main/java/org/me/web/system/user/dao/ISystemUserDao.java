7d309df54c40cdda44d9f2681bb25a215d712a86
845642761@qq.com <845642761@qq.com>
2016-08-12 18:20:41 +0800

    用户dao优化

--- src/main/java/org/me/web/system/user/dao/ISystemUserDao.java
+++ src/main/java/org/me/web/system/user/dao/ISystemUserDao.java
@@ -6,13 +6,13 @@ import org.apache.ibatis.annotations.Param;
 import org.me.web.system.user.entity.SystemUser;
 
 public interface ISystemUserDao{
-	public int insert(SystemUser user);
 	public List<SystemUser> getList(SystemUser user);
 	public int getListSize(SystemUser user);//查询列表大小
-	public int updateUserState(@Param("strUserId")String strUserId, @Param("nState")Integer nState);//更改用户状态
 	public SystemUser getById(String strUserId);
+	public int updateUserState(@Param("strUserId")String strUserId, @Param("nState")Integer nState);//更改用户状态
 	public SystemUser getByLoginName(String strLoginName);
 	
 	public int delById(String strUserId);
+	public int insert(SystemUser user);
 	public int update(SystemUser user);
 }
